a6c9117ad09cf59c07819c84920ca13970f4daa2
Danillo <danillodesouza03@gmail.com>
2023-04-14 11:56:38 -0300

    Create 04 - nomenclaturas.js
    Uso de camelCase, e boas práticas para declarar variáveis.

---
+++ 09 - variaveis/04 - nomenclaturas.js
@@ -0,0 +1,10 @@
+// Começar com letra, $, _, 
+// se for uma classe ou função construtora pode começar com maiúscula
+// geralmente quando é uma constante usamos todas letras maiúsculas
+// camelCase
+
+var var1 = 10;
+
+let testeVariavel = 'teste';
+
+const TESTE = 10;
