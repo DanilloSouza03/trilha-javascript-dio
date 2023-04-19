847494b42546a302733d33842c1c86f8dd7e56e9
Danillo <danillodesouza03@gmail.com>
2023-04-19 10:46:14 -0300

    Create 01 - tipos.js

---
+++ 10 - tipos de dados/01 - tipos.js
@@ -0,0 +1,29 @@
+
+/* 
+    boolean
+    null
+    undefined
+    number
+    string
+    synbol
+*/
+
+// object
+
+let x = 10;
+x = 20;
+
+let y = { numero: 10};
+y.numero = 20;
+
+console.log(10 + 10);
+console.log(10 + '10');
+console.log(10 - '10');
+console.log(10 - 'texto');
+
+console.log('--------------------------')
+const x1 = { nome: null};
+
+console.log(x1.nome);
+console.log(x1.idade);
+
