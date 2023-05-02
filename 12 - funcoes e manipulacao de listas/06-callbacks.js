acceadda4198083fa3d6a4c51ad33676b29af126
Danillo <danillodesouza03@gmail.com>
2023-05-02 11:56:25 -0300

    Create 06-callbacks.js

---
+++ 12 - funcoes e manipulacao de listas/06-callbacks.js
@@ -0,0 +1,16 @@
+
+function adicao(x, y) {
+    return x + y
+}
+
+function multiplicacao(x, y) {
+    return x * y
+}
+
+function calcular(x, operacao, y) {
+    console.log(operacao(x, y))
+}
+
+calcular(10, adicao, 20)
+calcular(10, multiplicacao, 20)
+
