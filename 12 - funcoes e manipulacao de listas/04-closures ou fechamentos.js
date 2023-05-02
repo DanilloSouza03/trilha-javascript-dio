0a7ce1bfd0bf40360e72ec63f5530da4401afd25
Danillo <danillodesouza03@gmail.com>
2023-05-02 08:57:34 -0300

    Create 04-closures ou fechamentos.js

---
+++ 12 - funcoes e manipulacao de listas/04-closures ou fechamentos.js
@@ -0,0 +1,13 @@
+// Closures
+
+function soma(x) {
+    return (y) => {
+        return x + y;
+    }
+}
+
+const somaParcial = soma(10)
+
+console.log(somaParcial(20))
+console.log(somaParcial(30))
+console.log(somaParcial(40))
